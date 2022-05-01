22f23633303a11690959821522af7ceadb9b7295
Santurde99 <104498579+Santurde99@users.noreply.github.com>
2022-05-01 18:14:24 +0200

    Add files via upload

--- Jugador.java
+++ Jugador.java
@@ -1,13 +1,39 @@
 package packUno;
 
+import java.util.Iterator;
+
 public class Jugador {
 
 	//Atributos
+	private int numeroDeJugador;
+	private ListaCartas lista;
 	
 	//Metodos
 	
+	public Jugador(int pNum) {
+		this.numeroDeJugador = pNum;
+		lista = new ListaCartas();
+	}
 	
+	public void ejecutarTurno() {
+		
+	}
 	
+	private void darOpcionDeRobar() {
+		System.out.println("(0) Robar carta");
+	}
+	
+	private Iterator<Carta> getIterador() { 	//Creo que falla porque aun no esta la lista de Cartas ni las cartas
+		return this.lista.iterator();
+	}
+	
+	public void robarCarta() {
+		
+	}
+	
+	public void robarCartasInicio() {
+		
+	}
 	
 	
 	
--- ListaJugadores.java
+++ ListaJugadores.java
@@ -1,5 +1,55 @@
 package packUno;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class ListaJugadores {
 
+	//Atributos	
+	private ArrayList<Jugador> lista;
+	private static ListaJugadores milistaJugadores=new ListaJugadores();
+
+	//Metodos
+	private  ListaJugadores() {
+		this.lista = new ArrayList<Jugador>();
+	}
+	
+	public static ListaJugadores getListaJugadores() {
+		return milistaJugadores;
+	}
+	
+	private Iterator<Jugador> getIterador() {
+		return this.lista.iterator();
+	}
+    
+	public void anadirJugadores(int pNumJugadores) {  //Añade los jugadores con su numero, los añade en sentido inverso es decir si fueran cuatro el orden seria (4,3,2,1)
+		int i = pNumJugadores;
+		while (i > 0) {		
+			this.lista.add(new Jugador(pNumJugadores));
+			i = i -1;
+		} 
+	}
+	
+	public void pasarTurno(int pNumTurnos) {		//Ignorar de momento
+		Iterator<Jugador> itr=this.getIterador();
+		Jugador unJugador = null;
+		while (itr.hasNext()) {
+			unJugador.ejecutarTurno();
+		
+		}
+		
+
+	}
+	
+	public void robarCartasInicio() {		//hace un loop en el que ejecuta el metodo de robar las cartas iniciales de todos los jugadores de la lista
+		Iterator<Jugador> itr=this.getIterador();
+		Jugador unJugador = null;
+		while (itr.hasNext()) {
+			unJugador = itr.next();
+			unJugador.robarCartasInicio();
+		}
+	}
+	
+	
+ 
 }
